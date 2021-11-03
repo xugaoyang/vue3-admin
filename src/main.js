d0bb0abdd30dbcf644db06722af76bd71884f8eb
YANNのLAPTOP\yann <yann_xu@outlook.com>
2021-11-03 15:36:13 +0800

    styles: use sass change element-plus color

--- src/main.js
+++ src/main.js
@@ -2,14 +2,9 @@ import { createApp } from 'vue'
 import router from './router'
 import store from './store'
 import './assets/styles/index.css'
-
-// import './styles/element/index.scss'
+import './styles/element/index.scss'
 import ElementPlus from 'element-plus'
-
-
-import 'element-plus/dist/index.css'
-// import './theme/index.css'
-
+// import 'element-plus/dist/index.css'
 import { Setting, Sunny, Moon } from '@element-plus/icons'
 import App from './App.vue'
 
