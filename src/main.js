1bd132bc415f71c5e43d1603a1fb29b106d2ba6b
YANNのLAPTOP\yann <yann_xu@outlook.com>
2021-11-04 16:32:55 +0800

    styles: remove useless css

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import router from './router'
 import store from './store'
-import './assets/styles/index.css'
+import './assets/styles/index.scss'
 import './styles/element/index.scss'
 import ElementPlus from 'element-plus'
 // import 'element-plus/dist/index.css'
